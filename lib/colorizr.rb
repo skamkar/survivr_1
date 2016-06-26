f43a6618372f96c23b4d182c85870e0f76895ec2
skamkar <sjk@zestfinance.com>
2016-06-26 15:51:44 -0500

    Add basic functionality

---
+++ lib/colorizr.rb
@@ -0,0 +1,36 @@
+class String
+
+  attr_reader :colors_hash
+
+  @@colors_hash = {
+      :red => "31m",
+      :green => "32m",
+      :yellow => "33m",
+      :blue => "34m",
+      :pink => "35m",
+      :light_blue => "94m",
+      :white => "97m",
+      :light_grey => "37m",
+      :black => "30m"
+  }
+
+  def self.colors
+    @@colors_hash.keys
+  end
+
+  def self.sample_colors
+    @@colors_hash.each do |color, format|
+      puts "I am " + "#{color}".send("#{color.to_s}") + "."
+    end
+  end
+
+  def self.create_colors
+    @@colors_hash.each do |color, format|
+      self.send(:define_method, "#{color}") do
+          return "\e[#{format}#{self}\e[0m"
+      end
+    end
+  end
+
+end
+
